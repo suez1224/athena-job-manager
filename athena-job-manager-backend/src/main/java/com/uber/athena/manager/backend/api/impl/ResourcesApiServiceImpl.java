6ce0d68377c7273a8985aa25fd01ac1f2e948fdd
Rong Rong <walter_ddr@hotmail.com>
2019-05-11 09:32:39 -0700

    swagger redesign initial step, adding in job and resource API

--- athena-job-manager-backend/src/main/java/com/uber/athena/manager/backend/api/impl/ResourcesApiServiceImpl.java
+++ athena-job-manager-backend/src/main/java/com/uber/athena/manager/backend/api/impl/ResourcesApiServiceImpl.java
@@ -1,8 +1,8 @@
 package com.uber.athena.manager.backend.api.impl;
 
 import com.uber.athena.manager.backend.api.ApiResponseMessage;
-import com.uber.athena.manager.backend.api.JobsApiService;
 import com.uber.athena.manager.backend.api.NotFoundException;
+import com.uber.athena.manager.backend.api.ResourcesApiService;
 
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.SecurityContext;
@@ -11,31 +11,17 @@ import java.util.UUID;
 @javax.annotation.Generated(
     value = "io.swagger.codegen.languages.JavaJerseyServerCodegen",
     date = "2019-05-11T09:18:17.249-07:00")
-public class JobsApiServiceImpl extends JobsApiService {
+public class ResourcesApiServiceImpl extends ResourcesApiService {
 
   @Override
-  public Response deployJob(SecurityContext securityContext)
+  public Response createResource(SecurityContext securityContext)
       throws NotFoundException {
     // do some magic!
     return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
   }
 
   @Override
-  public Response getJob(UUID jobUUID, SecurityContext securityContext)
-      throws NotFoundException {
-    // do some magic!
-    return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
-  }
-
-  @Override
-  public Response listJob(String serviceName, SecurityContext securityContext)
-      throws NotFoundException {
-    // do some magic!
-    return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
-  }
-
-  @Override
-  public Response terminateJob(UUID jobUUID, SecurityContext securityContext)
+  public Response listResources(UUID jobUUID, SecurityContext securityContext)
       throws NotFoundException {
     // do some magic!
     return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
